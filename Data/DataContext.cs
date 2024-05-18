e3dae4dac8692ea062853b9434020e8278b1e852
thali <thali@DESKTOP-98039HM>
2024-05-18 20:08:26 -0300

    Adicionar arquivos de projeto.

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityStudy.Data;
+
+public class DataContext : IdentityDbContext
+{
+    public DataContext(DbContextOptions<DataContext> options)
+    {
+
+    }
+}
+
